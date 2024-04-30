6e951dfe6cd07c64dca91b5361219698ec9befed
sahyan <tahirihasinarakotomanga@gmail.com>
2024-04-30 16:34:30 +0300

    documentation de la fonction de création du chemin de notre curseur

--- src/move.py
+++ src/move.py
@@ -1,11 +1,24 @@
 import turtle
 
-def readPath(filename: str):
+def readPath(filename: str) -> list[str]:
+    """
+        Read a text file and create a list containing the data from the file.
+        
+        parameter:
+            filename (str) : containing the path to the file to be read
+            
+        example :
+            filename = "./test.txt"
+            readPath(filename)
+
+    
+    """
     path = []
     brute = [line.strip() for line in open(filename, 'r')]
     for i in brute:
         path.append(i.split())
     return path
+
 
 def forward(curseur, distance=0):
     curseur.forward(distance)
